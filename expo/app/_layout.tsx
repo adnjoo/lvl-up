4703f54ef092ad87c6907811a90583dc92c46e46
Andrew Njoo <adnjoo@gmail.com>
2025-03-06 23:59:04 +0700

    _l

---
+++ expo/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        // Hide the header for all other routes.
+        headerShown: false,
+      }}
+    />
+  );
+}
