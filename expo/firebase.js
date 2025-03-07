c2596bd5d0739a6f263870b3ebdaeab5c9d68521
Andrew Njoo <adnjoo@gmail.com>
2025-03-07 09:53:08 +0700

    rm firebase analytics

--- expo/firebase.js
+++ expo/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
@@ -26,10 +25,9 @@ const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
-const analytics = getAnalytics(app);
 
 // Firebase services
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { app, analytics, auth, db, storage };
+export { app, auth, db, storage };
