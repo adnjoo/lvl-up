112f530cd043a0c98cfdf773289addd712881867
Andrew Njoo <adnjoo@gmail.com>
2025-03-11 23:35:47 +0700

    heading

--- expo/tailwind.config.js
+++ expo/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'spacegrotesk-regular': 'SpaceGrotesk-Regular',
+        'spacegrotesk-medium': 'SpaceGrotesk-Medium',
+        'spacegrotesk-bold': 'SpaceGrotesk-Bold',
+      },
+    },
   },
   plugins: [],
 };
