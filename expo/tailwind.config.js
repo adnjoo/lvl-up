2eff3823762a78cc7a6bc40d457abc3ca8700aac
Andrew Njoo <adnjoo@gmail.com>
2025-03-12 20:35:10 +0700

    tw

--- expo/tailwind.config.js
+++ expo/tailwind.config.js
@@ -12,6 +12,33 @@ module.exports = {
         'manrope-regular': 'Manrope-Regular',
         'manrope-light': 'Manrope-Light',
       },
+      colors: {
+        brand: {
+          DEFAULT: '#3B82F6',
+          background: '#0F172A',
+          outline: '#60A5FA',
+        },
+        neutral: {
+          DEFAULT: '#6B21A8',
+          background: '#1E1B4B',
+          outline: '#A78BFA',
+        },
+        success: {
+          DEFAULT: '#22C55E',
+          background: '#DCFCE7',
+          outline: '#86EFAC',
+        },
+        error: {
+          DEFAULT: '#DC2626',
+          background: '#FEE2E2',
+          outline: '#FCA5A5',
+        },
+        warning: {
+          DEFAULT: '#F59E0B',
+          background: '#FFF7EB',
+          outline: '#FCD34D',
+        },
+      },
     },
   },
   plugins: [],
